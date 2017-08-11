f8216df6cc0b216cf38cb80291f22208cb3a6f5c
Tony Ebel <tonyebel123@gmail.com>
2017-08-11 00:43:17 -0500

    Landing page now uses jQuery's AJAX method to send user's input via POST. Request goes to getTemp.php and returned data will be parsed and filled into inner section element. Created getTemp.php which will hit API (openweathermap.org) and return with JSON formatted data.

--- public/index.php
+++ public/index.php
@@ -1,29 +1,60 @@
 <!doctype html>
 <html lang="en-US">
-    <head>
-        <!-- Meta -->
-        <meta charset="utf-8">
-        <title>Temp App</title>
-        
-    <script>
-        function lookupTemp() {
-            console.log("looking up temp...");
-        }
-    </script>
-    </head>
+	<head>
+		<!-- Meta -->
+		<meta charset="utf-8">
+		<title>Temp App</title>
 
-    <body>
-        <header>
-            <h1>Temperature App</h1>
-        </header>
+		<!-- Include jQuery Google CDN -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+		<script>
+			function btnPress(){
+				/* Validation will go here */
 
-        <section>
-            <form action="">
-                <input id="inCity" placeholder="Search a location..." type="search" maxlength="35" autofocus>
-                <input id="inSubmit" type="submit" onclick="lookupTemp();">
-            </form>
-        </section>
+				$.post(document.location.href + 'getTemp.php',
+					{
+						location: $("#inLocation").val()
+					},
+					function(data, httpStatus){
+						if (httpStatus === "success"){
+							console.log('good');
+							$("#outContent").html(data);
+						} else {
+							console.log('bad');
+							$("#outContent").html("\n<p>Oops! An error occured</p>\n<br>\n<p>Status: " + httpStatus + "</p>\n");
+						};
+					}
+				);
+				
+				$("#inLocation").val('');
+				$("#inLocation").focus();
+			}
+			function checkEnter(e){
+				var charCode;
+				if (e && e.which){
+					charCode = e.which;
+				};
+				if (charCode == 13){
+					btnPress();
+				} else {
+					return true;
+				};
+			}
+		</script>
+	</head>
 
-    </body>
+	<body>
+		<header>
+			<h1>Temperature App</h1>
+		</header>
 
-</html>
+		<section>
+			<input id="inLocation" placeholder="Search a location..." type="text" maxlength="35" autofocus onkeypress="checkEnter(event);">
+			<input id="inSearch" type="button" value="Update" onclick="btnPress();">
+
+			<section id="outContent">...</section>
+		</section>
+
+	</body>
+
+</html> 
