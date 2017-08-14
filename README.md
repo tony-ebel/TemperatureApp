06693abaefb4919e9a5f8f7ac85d64ba9db0cec2
Tony Ebel <tonyebel123@gmail.com>
2017-08-14 01:11:32 -0500

    Final touches... Moved index.php to index.html Adjusted content injected into landing page Server side validation now returns nothing on failure Cleaned up all files and added more comments Added minimal css Updated README

--- README.md
+++ README.md
@@ -1,8 +1,35 @@
 ##### Simple Temperature App #####
 
 
-Simple web application that accepts a location as input and injects the current temperature for the location into the page. *Includes vagrant vm (Ubuntu 14.05)*
+Simple web application that accepts a location as input and displays current temperature into the page.
+*Includes vagrant vm (Ubuntu 14.05)*
+
+HOW TO:
+
+	1. Clone this Git repo
+	2. In terminal start vm with vagrant from repo directory (vagrant up)
+	3. In browser visit http://192.168.33.10/ or http://localhost:8000/
+
+PROCESS:
+
+	1. Client enters location
+	2. Validation client side on input
+	3. AJAX calls getTemp.php as POST request
+	4. Validation server side on POST data
+	5. PHP calls API with curl as GET request
+	6. Returns JSON from API to client
+	7. Client parses returned data and injects into page
+
+REQUIRES:
+
+	* Vagrant
+	* VM Hypervisor
 
 
-*Will add more info and instructions here!!!*
-
+Examples of valid input:
+	
+	1. Omaha
+	2. Omaha, NE
+	3. Overland Park
+	4. Overland Park, KS
+	5. London, GB
--- public/getTemp.php
+++ public/getTemp.php
@@ -1,17 +1,28 @@
 <?php
 	/* Validation (Server Side)*/
+
+	//ensure POST request and data includes location variable
 	if(!$_POST || !$_POST['location']){
-		exit('{"cod":400,"message":"No location given"}');
+		exit(1); //this likely isn't from client, so exit and don't return why
+
+	//regex to validate location variable incase request is not from client, or client validation was tampered
 	} elseif (!preg_match("/^[a-zA-Z][a-zA-Z]+\s*[a-zA-Z]+(?:,\s[a-zA-Z]{2})*$/", $_POST['location'])){
-		exit('{"cod":400,"message":"Invalid location"}');
+		exit(1); //still likely not from client, exit without returning why
+
+	//validation passed. encode/assign and continue
 	} else {
 		$location = urlencode($_POST['location']);
 	}
 
-	$url = "http://api.openweathermap.org/data/2.5/weather?q={$location}&units=imperial&APPID=141074e8a2059d7ec47d6e662cd5089a";
+
+	/* Call API with curl */
+
+	//build url string with $location as query and API key
+	$url = "https://api.openweathermap.org/data/2.5/weather?q={$location}&units=imperial&APPID=141074e8a2059d7ec47d6e662cd5089a";
 
 	// create curl resource
 	$request = curl_init();
+
 	// set curl options
 	curl_setopt($request, CURLOPT_URL, $url);
 	curl_setopt($request, CURLOPT_RETURNTRANSFER, 1);
@@ -21,5 +32,7 @@
 	// close connection
 	curl_close($request);
 
+	//return JSON string to client
 	echo($data);
+	exit(0);
 ?>
